c867a451171c5196182588665f75ec73e50980a2
Logan Farci <farci.logan@gmail.com>
2021-05-30 14:30:27 +0200

    The script can write to a new file given some command line argument

--- migration.py
+++ migration.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
 
 import csv
-import datetime
 from sodexo import make_sodexo_transaction
-from parse import parse
+from argparse import ArgumentParser
 
-COLUMNS = 3
-DESCRIPTION_INDEX = 1
-AMOUNT_INDEX = 2
-path = "/home/logan/Documents/data_sodexo.csv"
+fieldnames = ["date", "source", "destination", "amount", "category"]
+
+def write_transaction(source, target):
+    with open(source, "r") as input, open(target, "w+") as output:
+        writer = csv.DictWriter(output, fieldnames=fieldnames)
+        reader = csv.reader(input, delimiter=';')
+        writer.writeheader()
+        next(reader)
+        for row in reader:
+            transaction = make_sodexo_transaction(row)
+            writer.writerow(transaction)
 
 def main():
-    with open(path) as csvfile:
-        index = 0
-        reader = csv.reader(csvfile, delimiter=';')
-        for row in reader:
-            if index == 0:
-                row[1] = "Source"
-                row.append("Destination")
-                row.append("Category")
-            else:
-                transaction = make_sodexo_transaction(row)
-                print(transaction)
-            index += 1
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--file", nargs=1, help="path to the source CSV file.", required=True)
+    parser.add_argument("-d", "--destination", nargs=1, help="path to the destination CSV file.", required=True)
+    parser.add_argument("-t", "--type", nargs=1, help="source type of the input file.", required=False, default="sodexo", choices=["sodexo", "bank"])
+    args = parser.parse_args()
+
+    if args.type[0] == "sodexo":
+        write_transaction(args.file[0], args.destination[0])
+        print(f"Sodexo file at {args.file[0]} migrated and written to {args.destination[0]}")
+    else:
+        print("Unsupported file type.")
 
 if __name__ == "__main__":
     main()
